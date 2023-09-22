5df0ef160cc17d523ed6bed912f853b14f951c7c
Matt Plitnikas <mplitnikas@gmail.com>
2023-09-21 23:58:22 -0400

    set 1 challenge 3

--- src/set1.rs
+++ src/set1.rs
@@ -3,11 +3,6 @@ pub mod utils {
     use base64::{engine::general_purpose, Engine as _};
     use hex;
 
-    // pub fn hex_string_to_b64_string(data: &str) -> String {
-    //     let bytes = hex_to_bytes(data);
-    //     bytes_to_b64(&bytes)
-    // }
-
     pub fn b64_to_bytes(data: &str) -> Vec<u8> {
         general_purpose::STANDARD_NO_PAD
             .decode(data)
@@ -27,11 +22,48 @@ pub mod utils {
     pub fn xor_bytes(a: &[u8], b: &[u8]) -> Vec<u8> {
         let mut buf: Vec<u8> = vec![];
 
-        for (x, y) in a.iter().zip(b.iter()) {
+        for (x, y) in a.iter().zip(b.iter().cycle()) {
             buf.push(x ^ y);
         }
 
         buf
+    }
+
+    pub fn score_letter_frequency(data: &str) -> u8 {
+        // returns range of 0-26
+        let common_letters = "etaoinshrdlucmwfgypbvkjxqz".as_bytes();
+        let mut score = 0;
+        let filtered_text: Vec<char> = data
+            .to_lowercase()
+            .chars()
+            .filter(|&c| c >= 'a' && c <= 'z')
+            .collect();
+        let filtered_len = filtered_text.len();
+
+        for char in filtered_text {
+            let freq = common_letters.iter().position(|&c| c == char as u8);
+            if let Some(value) = freq {
+                score += 26 - value;
+            }
+        }
+        score = score / (filtered_len + 1);
+        score.saturating_sub(data.len() - filtered_len) as u8
+    }
+
+    pub fn find_single_byte_xor_decode(data: &[u8]) -> (String, u8) {
+        let mut score = 0;
+        let mut out_string = vec![];
+        let mut char = 0;
+        for byte in 0..=255 {
+            let output = xor_bytes(data, &[byte]);
+            let current_score = score_letter_frequency(&String::from_utf8_lossy(&output));
+            if current_score > score {
+                score = current_score;
+                out_string = output;
+                char = byte;
+            }
+        }
+        (String::from_utf8_lossy(&out_string).to_string(), char)
     }
 }
 
@@ -58,5 +90,27 @@ mod tests {
 
         let xord = bytes_to_hex(&xor_bytes(&hex_to_bytes(hex1), &hex_to_bytes(hex2)));
         assert_eq!(xord, expected);
+    }
+
+    #[test]
+    fn test_letter_freq() {
+        let english_score = score_letter_frequency(
+            "It was the best of times, it was the worst of times! Incredible really.",
+        );
+        let gibberish_score = score_letter_frequency(
+            "dkbfsxdk.bnrslbnpoeasgpsreblnkxcb,mxkcvb289tp924lkewbkmbnblkeai329k42jporbprsdbnrsdlktnribtu",
+        );
+
+        assert!(english_score > gibberish_score);
+    }
+
+    #[test]
+    fn challenge_3() {
+        let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+        let bytes = hex_to_bytes(input);
+        let (output, chr) = find_single_byte_xor_decode(&bytes);
+
+        assert_eq!(output, "Cooking MC's like a pound of bacon");
+        assert_eq!(chr, 88);
     }
 }
